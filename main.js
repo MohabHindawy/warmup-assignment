60614faf00ec2908717f3f07b7abb42e88effc4e
MohabHindawy <mohab.hindawy@student.giu-uni.de>
2026-03-02 16:29:51 +0200

    first two functions

--- main.js
+++ main.js
@@ -7,7 +7,52 @@ const fs = require("fs");
 // Returns: string formatted as h:mm:ss
 // ============================================================
 function getShiftDuration(startTime, endTime) {
-    // TODO: Implement this function
+    const start = startTime.toLowerCase().split(' ')
+    const startTimeClean = start[0].split(':')
+
+    let startH = parseFloat(startTimeClean[0])
+    let startM = parseFloat(startTimeClean[1])
+    let startS = parseFloat(startTimeClean[2])
+
+    if(start[1] == 'pm' && startH != 12) {
+        startH += 12
+    }
+    if(start[1] == 'am' && startH == 12) {
+        startH = 0
+    }
+    
+    const end = endTime.toLowerCase().split(' ')
+    const endTimeClean = end[0].split(':')
+
+    let endH = parseFloat(endTimeClean[0])
+    let endM = parseFloat(endTimeClean[1])
+    let endS = parseFloat(endTimeClean[2])
+
+    if(end[1] == 'pm' && endH != 12) {
+        endH += 12
+    }
+    if(end[1] == 'am' && endH == 12) {
+        endH = 0
+    }
+
+    let seconds = endS - startS
+    if(seconds < 0) {
+        seconds += 60
+        endM--
+    }
+    let minutes = endM - startM
+    if(minutes < 0) {
+        minutes += 60
+        endH--
+    }
+    let hours = endH - startH
+    if(hours < 0) {
+        hours += 24
+    }
+    const secs = String(seconds).padStart(2, '0')
+    const mins = String(minutes).padStart(2, '0')
+
+    return `${hours}:${mins}:${secs}`
 }
 
 // ============================================================
@@ -17,7 +62,101 @@ function getShiftDuration(startTime, endTime) {
 // Returns: string formatted as h:mm:ss
 // ============================================================
 function getIdleTime(startTime, endTime) {
-    // TODO: Implement this function
+    const start = startTime.toLowerCase().split(' ')
+    const startTimeClean = start[0].split(':')
+
+    let startH = parseFloat(startTimeClean[0])
+    let startM = parseFloat(startTimeClean[1])
+    let startS = parseFloat(startTimeClean[2])
+
+    if(start[1] == 'pm' && startH != 12) {
+        startH += 12
+    }
+    if(start[1] == 'am' && startH == 12) {
+        startH = 0
+    }
+    
+    const end = endTime.toLowerCase().split(' ')
+    const endTimeClean = end[0].split(':')
+
+    let endH = parseFloat(endTimeClean[0])
+    let endM = parseFloat(endTimeClean[1])
+    let endS = parseFloat(endTimeClean[2])
+
+    if(end[1] == 'pm' && endH != 12) {
+        endH += 12
+    }
+    if(end[1] == 'am' && endH == 12) {
+        endH = 0
+    }
+
+    let seconds = 0
+    let minutes = 0
+    let hours = 0
+
+    if(startH < 8) {
+        let boundH = 8
+        let boundM = 0
+        let boundS = 0
+        if(endH < 8) {
+            boundH = endH
+            boundM = endM
+            boundS = endS
+        }
+        let s = boundS - startS
+        if(s < 0) {
+            s += 60
+            boundM--
+        }
+        let m = boundM - startM
+        if(m < 0) {
+            m += 60
+            boundH--
+        }
+        let h = boundH - startH
+
+        seconds += s
+        minutes += m
+        hours += h
+    }
+    if(endH >= 22) {
+        let boundH = 22
+        let boundM = 0
+        let boundS = 0
+        if(startH >= 22) {
+            boundH = startH
+            boundM = startM
+            boundS = startS
+        }
+        let s = endS - boundS
+        if(s < 0) {
+            s += 60
+            endM--
+        }
+        let m = endM - boundM
+        if(m < 0) {
+            m += 60
+            endH--
+        }
+        let h = endH - boundH
+
+        seconds += s
+        minutes += m
+        hours += h
+    }
+
+    if(seconds < 0) {
+        seconds -= 60
+        endM--
+    }
+    if(minutes < 0) {
+        minutes -= 60
+        endH--
+    }
+    const secs = String(seconds).padStart(2, '0')
+    const mins = String(minutes).padStart(2, '0')
+
+    return `${hours}:${mins}:${secs}`
 }
 
 // ============================================================
