3314e7a100ac30dde7d9f88b17f554a84e0dcddf
MohabHindawy <mohab.hindawy@student.giu-uni.de>
2026-03-05 15:14:29 +0200

    till 7 done

--- main.js
+++ main.js
@@ -40,11 +40,13 @@ function getShiftDuration(startTime, endTime) {
         seconds += 60
         endM--
     }
+    
     let minutes = endM - startM
     if(minutes < 0) {
         minutes += 60
         endH--
     }
+
     let hours = endH - startH
     if(hours < 0) {
         hours += 24
@@ -103,16 +105,19 @@ function getIdleTime(startTime, endTime) {
             boundM = endM
             boundS = endS
         }
+
         let s = boundS - startS
         if(s < 0) {
             s += 60
             boundM--
         }
+
         let m = boundM - startM
         if(m < 0) {
             m += 60
             boundH--
         }
+
         let h = boundH - startH
 
         seconds += s
@@ -133,11 +138,13 @@ function getIdleTime(startTime, endTime) {
             s += 60
             endM--
         }
+
         let m = endM - boundM
         if(m < 0) {
             m += 60
             endH--
         }
+
         let h = endH - boundH
 
         seconds += s
@@ -166,7 +173,39 @@ function getIdleTime(startTime, endTime) {
 // Returns: string formatted as h:mm:ss
 // ============================================================
 function getActiveTime(shiftDuration, idleTime) {
-    // TODO: Implement this function
+    const duration = shiftDuration.split(':')
+
+    let durationH = parseFloat(duration[0])
+    let durationM = parseFloat(duration[1])
+    let durationS = parseFloat(duration[2])
+
+    const idle = idleTime.split(':')
+
+    let idleH = parseFloat(idle[0])
+    let idleM = parseFloat(idle[1])
+    let idleS = parseFloat(idle[2])
+
+    let seconds = durationS - idleS
+    if(seconds < 0) {
+        seconds += 60
+        durationM--
+    }
+
+    let minutes = durationM - idleM
+    if(minutes < 0) {
+        minutes += 60
+        durationH--
+    }
+
+    let hours = durationH - idleH
+    if(hours < 0) {
+        hours += 24
+    }
+
+    const secs = String(seconds).padStart(2, '0')
+    const mins = String(minutes).padStart(2, '0')
+
+    return `${hours}:${mins}:${secs}`
 }
 
 // ============================================================
@@ -176,7 +215,14 @@ function getActiveTime(shiftDuration, idleTime) {
 // Returns: boolean
 // ============================================================
 function metQuota(date, activeTime) {
-    // TODO: Implement this function
+    const duration = activeTime.split(':')
+    const dateClean = date.split('-')
+
+    if(dateClean[0] == "2025" && dateClean[1] == "04" && (parseInt(dateClean[2]) >= 10 && parseInt(dateClean[2]) <= 30)) {
+        return parseInt(duration[0]) >= 6
+    }
+    if(parseInt(duration[0]) > 8) return true
+    return parseInt(duration[0]) == 8 && parseInt(duration[1]) >= 24
 }
 
 // ============================================================
@@ -186,7 +232,49 @@ function metQuota(date, activeTime) {
 // Returns: object with 10 properties or empty object {}
 // ============================================================
 function addShiftRecord(textFile, shiftObj) {
-    // TODO: Implement this function
+    const content = fs.readFileSync(textFile, 'utf8').split('\n')
+    let lastSeenIndex = -1
+    for(let i = 0; i < content.length; i++) {
+        const line = content[i].trim()
+        
+        if(line == "") continue;
+
+        const cols = line.split(',')
+        const id = cols[0].trim()
+        const date = cols[2].trim()
+
+        if(shiftObj.driverID.trim() == id && shiftObj.date.trim() == date) return {}
+
+        if(shiftObj.driverID.trim() == id) lastSeenIndex = i
+    }
+    const shiftDuration = getShiftDuration(shiftObj.startTime, shiftObj.endTime)
+    const idleTime = getIdleTime(shiftObj.startTime, shiftObj.endTime)
+    const activeTime = getActiveTime(shiftDuration, idleTime)
+    const isQuotaMet = metQuota(shiftObj.date, activeTime)
+
+    
+    const newLine = `${shiftObj.driverID},${shiftObj.driverName},${shiftObj.date},${shiftObj.startTime},${shiftObj.endTime},${shiftDuration},${idleTime},${activeTime},${isQuotaMet},false`
+    if(lastSeenIndex == -1) {
+        content.push(newLine)
+    }
+    else {
+        content.splice(lastSeenIndex + 1, 0, newLine)
+    }
+
+    fs.writeFileSync(textFile, content.join('\n'), 'utf8')
+
+    return {
+        driverID: shiftObj.driverID,
+        driverName: shiftObj.driverName,
+        date: shiftObj.date,
+        startTime: shiftObj.startTime,
+        endTime: shiftObj.endTime,
+        shiftDuration: shiftDuration,
+        idleTime: idleTime,
+        activeTime: activeTime,
+        metQuota: isQuotaMet,
+        hasBonus: false
+    }
 }
 
 // ============================================================
@@ -198,7 +286,23 @@ function addShiftRecord(textFile, shiftObj) {
 // Returns: nothing (void)
 // ============================================================
 function setBonus(textFile, driverID, date, newValue) {
-    // TODO: Implement this function
+    const content = fs.readFileSync(textFile, 'utf8').split('\n')
+    for(let i = 0; i < content.length; i++) {
+        const line = content[i].trim()
+        
+        if(line == "") continue;
+
+        const cols = line.split(',')
+        const id = cols[0].trim()
+        const dateVal = cols[2].trim()
+
+        if(driverID == id && dateVal == date) {
+            const newLine = `${id},${cols[1].trim()},${dateVal},${cols[3].trim()},${cols[4].trim()},${cols[5].trim()},${cols[6].trim()},${cols[7].trim()},${cols[8].trim()},${newValue}`
+            content[i] = newLine
+            fs.writeFileSync(textFile, content.join('\n'), 'utf8')
+            return
+        }
+    }
 }
 
 // ============================================================
@@ -209,7 +313,25 @@ function setBonus(textFile, driverID, date, newValue) {
 // Returns: number (-1 if driverID not found)
 // ============================================================
 function countBonusPerMonth(textFile, driverID, month) {
-    // TODO: Implement this function
+    const content = fs.readFileSync(textFile, 'utf8').split('\n')
+    let count = 0
+    let found = false
+    for(let i = 0; i < content.length; i++) {
+        const line = content[i].trim()
+        
+        if(line == "") continue;
+
+        const cols = line.split(',')
+        const id = cols[0].trim()
+        const monthVal = parseInt(cols[2].trim().split('-')[1])
+        const hasBonus = cols[9].trim()
+
+        if(driverID == id) {
+            found = true
+            if(parseInt(month) == monthVal && hasBonus.toLowerCase() == "true") count++
+        }
+    }
+    return found ? count : -1
 }
 
 // ============================================================
